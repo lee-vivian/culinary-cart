c61a5ba7703fdc7455df45cbb57e06dd9234db3f
Vivian Lee <lee-vivian@users.noreply.github.com>
2017-10-08 13:39:21 -0400

    Edit steps and ingredients
    - Added edit steps functionality
    - Feature: Add steps dynamically added
    - Ingredients for edit ingredients functionality displayed
    - Added x-delete and add-ingredients buttons (onclick functions IP)

--- edit-recipe.php
+++ edit-recipe.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <?php
 
-  // Connect to SQL database
-  include("connect.php");
+// Connect to SQL database
+include("connect.php");
 
-  // Get recipe id
-  $recipe_id = $_GET['rid'];
+// Get recipe id
+$recipe_id = $_GET['rid'];
 
-  // Link to edit recipe
-  $editLink = "'update-recipe.php?rid=".$recipe_id."&action=edit'";
+// Link to edit recipe
+$editLink = "'update-recipe.php?rid=".$recipe_id."&action=edit'";
 
-  // Get recipe fields
-  $sql = sprintf('SELECT recipe_name, prep_time, cook_time, cuisine, diet_restriction,
-                  recipe_type, num_servings
-                  FROM recipes
-                  WHERE recipe_id = %s',$recipe_id);
+// Get recipe overview fields
+$sql = sprintf('SELECT recipe_name, prep_time, cook_time, cuisine, diet_restriction,
+  recipe_type, num_servings
+  FROM recipes
+  WHERE recipe_id = %d',$recipe_id);
 
   $query = mysqli_query($conn, $sql);
   if (!query) {
@@ -31,172 +31,254 @@
     $num_servings = $row['num_servings'];
   }
 
-  // Select input form function to select proper default value
-  function selectDefault($input, $actual) {
-    if ($input == $actual) {
-      $selected = ' selected';
-    }
-    else {
-      $selected = '';
-    }
-    echo "<option value = '" . $input . "'>" . $input . "</option>";
-  }
-?>
+  // Get recipe ingredients
+  $sql = sprintf('SELECT ingredient_name, quantity, unit_type, weight_unit,
+    volume_unit
+    FROM ingredients
+    WHERE recipe_id = %d',$recipe_id);
 
-<html>
-
-<head>
-
-  <title>Edit: <?php echo $recipe_name ?></title>
-
-  <link href = "style.css" type = "text/css" rel = "stylesheet">
-  <link href = "tabs.css" type = "text/css" rel = "stylesheet"></link>
-</head>
-
-<body>
-
-  <?php include("navbar.html") ?>
-
-  <div style="margin-left:18%;padding:1px 16px;height:1000px;">
-    <h1>Edit: <?php echo $recipe_name ?><br><br></h1>
-
-    <div class="tab">
-      <button class="tablinks" onclick="openRecipeTab(event, 'Overview')"
-        id="overview-tab">Overview</button>
-      <button class="tablinks" onclick="openRecipeTab(event, 'Ingredients')"
-        id = "ingredients-tab">Ingredients</button>
-      <button class="tablinks" onclick="openRecipeTab(event, 'Steps')"
-        id = "steps-tab">Steps</button>
-    </div>
-
-    <!-- Overview tab form -->
-
-    <div id="Overview" class="tabcontent">
-
-      <?php $editLink = substr($editLink,0,strlen($editLink)-1) . "&tab=overview'";
-      ?>
-
-      <form action= <?php echo $editLink?> method = "post">
-        <br>
-        <input type = "hidden" name = "recipeTab" value="overview">
-
-        Recipe Name:<br>
-          <input type = "text" name = "name" value = <?php echo "'" . $recipe_name . "'"?> required> <br><br>
-        Prep Time:<br>
-          <input type = "number" name = "prep" min = "0" value = <?php echo $prep_time?> required> min.<br><br>
-        Cook Time:<br>
-          <input type = "number" name = "cook" min = "0" value = <?php echo $cook_time?> required> min.<br><br>
-        Cuisine:<br>
-          <input type = "text" name = "cuisine" value = <?php echo "'" . $cuisine . "'"?>>
-        <br><br>
-        Dietary Restrictions:<br>
-        <select name = "diet">
-          <?php selectDefault("none", $diet_restriction)?>
-          <?php selectDefault("vegetarian", $diet_restriction)?>
-          <?php selectDefault("vegan", $diet_restriction)?>
-          <?php selectDefault("gluten free", $diet_restriction)?>
-          <?php selectDefault("kosher", $diet_restriction)?>
-          <?php selectDefault("pescetarian", $diet_restriction)?>
-          <?php selectDefault("nut allergies", $diet_restriction)?>
-       </select>
-        <br><br>
-        Recipe Type:<br>
-        <select name = "type">
-          <?php selectDefault("meal", $recipe_type)?>
-          <?php selectDefault("drink", $recipe_type)?>
-          <?php selectDefault("snack", $recipe_type)?>
-        </select>
-        <br><br>
-        Servings:<br>
-          <input type = "number" name = "servings" min = "1" value = <?php echo $num_servings?> required>
-        <br><br>
-
-        <input type = "submit" value = "Save Changes">
-        <input type = "reset" value = "Undo Changes">
-      </form>
-    </div>
-
-    <!-- Ingredients tab form -->
-
-    <?php
-
-    /*
-    // Get recipe ingredients array
-    $sql = sprintf('SELECT ingredient_name, quantity, unit_type, weight_unit, volume_unit
-                    FROM ingredients
-                    WHERE recipe_id = %s',$recipe_id);
-    $query = mysqli_query($conn, $sql);
-    if (!query) {
+    $ingredients = mysqli_query($conn, $sql);
+    if(!ingredients) {
       die ('SQL Error: ' . mysqli_error($conn));
     }
 
-    $ingredient_no = 0;
+    // Get recipe steps
+    $sql = sprintf('SELECT description
+      FROM steps
+      WHERE recipe_id = %d
+      ORDER BY step_num',$recipe_id);
 
-    while ($row = mysqli_fetch_array($query)) {
+      $steps = mysqli_query($conn, $sql);
+      if(!steps) {
+        die ('SQL Error: ' . mysqli_error($conn));
+      }
 
-      $iname = "'iname" . $ingredient_no . "'";
-
-      echo "<input type = 'text' name = iname" . $iname . "placeholder="Ingredient" required>"
-
-      $iname += 1;
-
-    // pass $ingredient_no into form action link
-
-    }
-    /*
-     ?>
-    <br><br>
-
-    <h3>Steps:</h3>
-
-    <?php
-    // Get recipe steps array
-    $sql = sprintf('SELECT step_num, description
-                    FROM steps
-                    WHERE recipe_id = %s
-                    ORDER BY step_num',$recipe_id);
-    $query = mysqli_query($conn, $sql);
-    if (!query) {
-      die ('SQL Error: ' . mysqli_error($conn));
-    }
-    while ($row = mysqli_fetch_array($query)) {
-
-    }
-    */
-     ?>
-     <br>
-     <button type = "button" onclick = "deleteRecipe('<?php echo $recipe_id?>','<?php echo $recipe_name?>')">Delete Recipe</button>
-
-    <script>
-      function deleteRecipe(recipe_id, recipe_name) {
-        var msg = "Are you sure you want to delete: " + recipe_name + "?";
-        var updateWindow = "update-recipe.php?rid=" + recipe_id + "&action=delete";
-        if (confirm(msg)) {
-          window.location = updateWindow;
+      // Select input form function to select proper default value
+      function selectDefault($input, $actual) {
+        if ($input == $actual) {
+          $selected = ' selected';
         }
         else {
-          window.location = "search-recipes.php";
+          $selected = '';
         }
+        echo "<option value = '" . $input . "'>" . $input . "</option>";
       }
+      ?>
 
-      function openRecipeTab(evt, recipeTab) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("tablinks");
-        for(i = 0; i < tablinks.length; i++) {
-          tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(recipeTab).style.display = "block";
-        evt.currentTarget.className += " active";
-      }
+      <html>
 
-      // Get the element with id = "overview-tab" and click on it
-      document.getElementById("overview-tab").click();
-    </script>
+      <head>
 
-  </div>
-</body>
-</html>
+        <title>Edit: <?php echo $recipe_name ?></title>
+
+        <link href = "style.css" type = "text/css" rel = "stylesheet">
+        <link href = "tabs.css" type = "text/css" rel = "stylesheet"></link>
+      </head>
+
+      <body>
+
+        <?php include("navbar.html") ?>
+
+        <div style="margin-left:18%;padding:1px 16px;height:1000px;">
+          <h1>Edit: <?php echo $recipe_name ?><br><br></h1>
+
+          <div class="tab">
+            <button class="tablinks" onclick="openRecipeTab(event, 'Overview')"
+            id="overview-tab">Overview</button>
+            <button class="tablinks" onclick="openRecipeTab(event, 'Ingredients')"
+            id = "ingredients-tab">Ingredients</button>
+            <button class="tablinks" onclick="openRecipeTab(event, 'Steps')"
+            id = "steps-tab">Steps</button>
+          </div>
+
+          <!-- Overview tab form -->
+
+          <div id="Overview" class="tabcontent">
+
+            <?php
+            $editOverviewLink = substr($editLink,0,strlen($editLink)-1) . "&tab=overview'";
+            ?>
+
+            <form action= <?php echo $editOverviewLink?> method = "post">
+              <br>
+
+              Recipe Name:<br>
+              <input type = "text" name = "name" value = <?php echo "'" . $recipe_name . "'"?> required> <br><br>
+              Prep Time:<br>
+              <input type = "number" name = "prep" min = "0" value = <?php echo $prep_time?> required> min.<br><br>
+              Cook Time:<br>
+              <input type = "number" name = "cook" min = "0" value = <?php echo $cook_time?> required> min.<br><br>
+              Cuisine:<br>
+              <input type = "text" name = "cuisine" value = <?php echo "'" . $cuisine . "'"?>>
+              <br><br>
+              Dietary Restrictions:<br>
+              <select name = "diet">
+                <?php selectDefault("none", $diet_restriction)?>
+                <?php selectDefault("vegetarian", $diet_restriction)?>
+                <?php selectDefault("vegan", $diet_restriction)?>
+                <?php selectDefault("gluten free", $diet_restriction)?>
+                <?php selectDefault("kosher", $diet_restriction)?>
+                <?php selectDefault("pescetarian", $diet_restriction)?>
+                <?php selectDefault("nut allergies", $diet_restriction)?>
+              </select>
+              <br><br>
+              Recipe Type:<br>
+              <select name = "type">
+                <?php selectDefault("meal", $recipe_type)?>
+                <?php selectDefault("drink", $recipe_type)?>
+                <?php selectDefault("snack", $recipe_type)?>
+              </select>
+              <br><br>
+              Servings:<br>
+              <input type = "number" name = "servings" min = "1" value = <?php echo $num_servings?> required>
+              <br><br>
+
+              <input type = "submit" value = "Save Changes">
+              <input type = "reset" value = "Undo Changes">
+            </form>
+          </div>
+
+          <!-- Ingredients tab form -->
+
+          <div id = "Ingredients" class = "tabcontent">
+
+            <?php
+            $editIngredientsLink = substr($editLink,0,strlen($editLink)-1) . "&tab=ingredients'";
+
+            $listOfIngredients = array();
+
+            while ($row = mysqli_fetch_array($ingredients)) {
+
+              if ($row['unit_type']=="weight") {
+                $unit = $row['weight_unit'];
+              }
+              elseif($row['unit_type']=="volume") {
+                $unit = $row["volume_unit"];
+              }
+              else {
+                $unit = "";
+              }
+
+              $listOfIngredients[] = array("ingredient_name"=>$row['ingredient_name'],
+              "quantity"=>$row['quantity'],
+              "unit"=>$unit);
+            }
+            ?>
+
+            <form action= <?php echo $editIngredientsLink?> method = "post">
+              <br>
+
+              <?php
+              foreach($listOfIngredients as $ingredients_array) {
+
+                if($ingredients_array['unit']!=""){
+                  $temp_unit = " " . $ingredients_array['unit'] . " ";
+                }
+                else {
+                  $temp_unit = " ";
+                }
+
+                echo "<button type = 'button' onclick = ''>X</button> " .
+                $ingredients_array['quantity'] . $temp_unit . $ingredients_array['ingredient_name'] .
+                "<br>";
+              }
+              ?>
+
+              <div id = "dynamicIngredientsInput">
+              </div>
+
+              <br>
+              <button type = "button" onclick = "addIngredient('dynamicStepsInput')">Add Ingredient</button>
+              <br>
+
+              <br><br>
+
+              <input type = "submit" value = "Save Changes">
+              <input type = "reset" value = "Undo Changes">
+            </form>
+
+          </div>
+
+          <!-- Steps tab form -->
+          <div id = "Steps" class = "tabcontent">
+
+            <?php
+            $editStepsLink = substr($editLink,0,strlen($editLink)-1) . "&tab=steps'";
+
+            $numSteps = 0;
+            $listOfSteps = array();
+
+            while ($row = mysqli_fetch_array($steps)) {
+              array_push($listOfSteps, $row['description']);
+            }
+            ?>
+
+            <form action= <?php echo $editStepsLink?> method = "post">
+              <br>
+              <?php
+              foreach($listOfSteps as $step) {
+                $numSteps = $numSteps + 1;
+                echo "Step " . $numSteps . ". <br><br> <textarea name = 'originalSteps[]' rows = '3'
+                cols = '80'>" . $step . "</textarea> <br><br>";
+              }
+              ?>
+
+              <div id = "dynamicStepsInput">
+              </div>
+
+              <button type = "button" onclick = "addStep('dynamicStepsInput')">Add Step</button>
+
+              <br><br>
+
+              <input type = "submit" value = "Save Changes">
+              <input type = "reset" value = "Undo Changes">
+            </form>
+
+          </div>
+
+          <!-- Delete Recipe button -->
+
+          <br>
+          <button type = "button" onclick = "deleteRecipe('<?php echo $recipe_id?>','<?php echo $recipe_name?>')">Delete Recipe</button>
+
+          <script>
+          function deleteRecipe(recipe_id, recipe_name) {
+            var msg = "Are you sure you want to delete: " + recipe_name + "?";
+            var updateWindow = "update-recipe.php?rid=" + recipe_id + "&action=delete";
+            if (confirm(msg)) {
+              window.location = updateWindow;
+            }
+            else {
+              window.location = "search-recipes.php";
+            }
+          }
+
+          var stepNumCounter = <?php echo $numSteps?>;
+
+          function addStep(divName) {
+            var newdiv = document.createElement('div');
+            newdiv.innerHTML = "Step " + (stepNumCounter++) +  ". <br><br><textarea name = 'newSteps[]' rows = '3' cols = '80'>";
+            document.getElementById(divName).appendChild(newdiv);
+          }
+
+          function openRecipeTab(evt, recipeTab) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+              tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for(i = 0; i < tablinks.length; i++) {
+              tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(recipeTab).style.display = "block";
+            evt.currentTarget.className += " active";
+          }
+
+          // Get the element with id = "overview-tab" and click on it
+          document.getElementById("overview-tab").click();
+          </script>
+
+        </div>
+      </body>
+      </html>
--- update-recipe.php
+++ update-recipe.php
@@ -28,29 +28,62 @@ if ($action == 'edit') {
     $num_servings = $_POST['servings'];
 
     $sql = sprintf('UPDATE recipes
-                    SET recipe_name = %s,
-                        prep_time = %d,
-                        cook_time = %d,
-                        cuisine = %s,
-                        diet_restriction = %s,
-                        recipe_type = %s,
-                        num_servings = %d
-                    WHERE recipe_id = %d',
-                    $recipe_name, $prep_time, $cook_time, $cuisine, $diet_restriction,
-                    $recipe_type, $num_servings, $recipe_id);
-    $query = mysqli_query($conn, $sql);
-    if (!query) {
-      die ('SQL Error: ' . mysqli_error($conn));
+      SET recipe_name = %s,
+      prep_time = %d,
+      cook_time = %d,
+      cuisine = %s,
+      diet_restriction = %s,
+      recipe_type = %s,
+      num_servings = %d
+      WHERE recipe_id = %d',
+      $recipe_name, $prep_time, $cook_time, $cuisine, $diet_restriction,
+      $recipe_type, $num_servings, $recipe_id);
+      $query = mysqli_query($conn, $sql);
+      if (!query) {
+        die ('SQL Error: ' . mysqli_error($conn));
+      }
+    }
+    elseif($tab == 'ingredients') {
+
+    }
+    elseif($tab == 'steps') {
+
+      $hasOldSteps = isset($_POST['originalSteps']);
+      $hasNewSteps = isset($_POST['newSteps']);
+
+      if ($hasOldSteps && $hasNewSteps) {
+        $total_steps = array_merge($_POST['originalSteps'],$_POST['newSteps']);
+      }
+      elseif($hasOldSteps) {
+        $total_steps = $_POST['originalSteps'];
+      }
+      else {
+        $total_steps = $_POST['newSteps'];
+      }
+
+      // Delete old steps for current $recipe_id
+      $sql = sprintf('DELETE FROM steps WHERE recipe_id = %d', $recipe_id);
+      $query = mysqli_query($conn, $sql);
+      if (!query) {
+        die ('SQL Error: ' . mysqli_error($conn));
+      }
+
+      // Insert new steps for current $recipe_id
+      $step_counter = 1;
+      foreach($total_steps as $s) {
+        if ($s!="") {
+          $sql = sprintf("INSERT INTO steps (`recipe_id`,`step_num`,`description`)
+          VALUES (%d, %d, '%s')", $recipe_id, $step_counter, $s);
+          $query = mysqli_query($conn,$sql);
+          if (!query) {
+            die ('SQL Error: ' . mysqli_error($conn));
+          }
+          $step_counter = $step_counter + 1;
+        }
+      }
     }
   }
-  elseif($tab == 'ingredients') {
 
-  }
-  elseif($tab == 'steps') {
-
-  }
-}
-
-// Jump back to search-recipes.php
-header("Location: search-recipes.php");
-?>
+  // Jump back to search-recipes.php
+  header("Location: search-recipes.php");
+  ?>
